d2b0d83a3213efeb3fe458c4ad5cbcc304d1c06e
STRIX\Phil <philmottin@gmail.com>
2023-01-06 22:43:57 +0200

    Background and player assets Basic Player controls script

---
+++ Pong/Assets/Scripts/playerControl.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerControl : MonoBehaviour
+{
+    public KeyCode moveUp;
+    public KeyCode moveDown;
+    public float speed = 10f;
+    public Rigidbody2D rb;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(moveUp)) {            
+            rb.velocity = new Vector2(0, speed);
+        } else if (Input.GetKey(moveDown)) {
+            rb.velocity = new Vector2(0, -speed);
+        } else {
+            rb.velocity = new Vector2(0, 0);
+        }
+    }
+}
